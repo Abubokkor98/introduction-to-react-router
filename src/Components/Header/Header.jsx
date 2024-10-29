e5b09e77902cb7e0d2d6c881c3043bf0c5a8f13d
Abubokkor98 <mail.abubokkor@gmail.com>
2024-10-29 22:35:36 +0600

    explore navlink

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import './Header.css';
 
 export default function Header() {
   return (
     <div>
-      <nav className="p-2 flex gap-7 text-xl bg-purple-700">
+      <nav className="p-2 flex gap-7 text-xl bg-purple-700 ">
         <span>My Website</span>
         <Link to={'/'}>Home</Link>
         <Link to={'/about'}>About</Link>
         <Link to={'/contact'}>Contact Us</Link>
-        <Link to={'/users'}>Users</Link>
-        <Link to={'/posts'}>Posts</Link>
+        <NavLink to={'/users'}>Users</NavLink>
+        <NavLink to={'/posts'}>Posts</NavLink>
       </nav>
     </div>
   );
